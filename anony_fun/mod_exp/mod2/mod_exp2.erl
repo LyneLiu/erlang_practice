0bd6cb6237e1c877b9c0381ad4e87933bbc3cc7d
LyneLiu <595596226@qq.com>
2015-11-06 14:49:29 +0800

    delete files

--- anony_fun/mod_exp/mod2/mod_exp2.erl
+++
@@ -1,7 +0,0 @@
--module(mod_exp2).
--compile(export_all).
-
-fun2() 	->
-	io:format("test 2.~n"),
-	FunVal3 = fun mod_exp3:fun3/0,
-	FunVal3().
--- anony_fun/mod_exp/mod2/mod_exp3.erl
+++
@@ -1,7 +0,0 @@
--module(mod_exp3).
--compile(export_all).
-
-fun3() 	->
-	io:format("test 3.~n"),
-	LoopFun = fun mod_exp1:fun1_1/0,
-	LoopFun().
